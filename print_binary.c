d6b3d88490bbde652dff9ac1e18f74f72bf20134
OnyeEbube <101593852+OnyeEbube@users.noreply.github.com>
2022-08-09 17:48:37 +0100

    Create print_binary.c

---
+++ print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary numbers
+ *
+ * Return: count
+ */
+
+int print_binary(int x)
+{
+  int count = 0;
+  int arr[32], i = 0;
+  
+  while (x / 2 > 2)
+  {
+    arr[i] = x % 2;
+    x = x / 2;
+    i++;
+  }
+  for (i; i >= 0; i--)
+  {
+    count += _putchar(arr[i] + '0');
+  }
+  return (count);
+}
