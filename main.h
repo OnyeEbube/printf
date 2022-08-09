baa81eae550a70dade464c4e2820ded5d0dbbe3a
OnyeEbube <101593852+OnyeEbube@users.noreply.github.com>
2022-08-09 22:19:46 +0100

    Update main.h

--- main.h
+++ main.h
@@ -46,7 +46,8 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 /* FUNCTIONS */
 int print_string(va_list list, char buf[], int flags, int width, int precision, int size);
 void print_buf(char buf[], int *n);
-
+int print_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
